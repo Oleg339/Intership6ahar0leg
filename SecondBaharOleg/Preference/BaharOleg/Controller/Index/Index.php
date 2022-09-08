9e7ab6a25e20361b374c09785f5e71477c8e8ae1
Oleg339 <44521022+Oleg339@users.noreply.github.com>
2022-09-08 14:10:39 +0300

    Update Index.php

--- SecondBaharOleg/Preference/BaharOleg/Controller/Index/Index.php
+++ SecondBaharOleg/Preference/BaharOleg/Controller/Index/Index.php
@@ -2,15 +2,33 @@
 
 namespace Amasty\SecondBaharOleg\Preference\BaharOleg\Controller\Index;
 
-use Amasty\BaharOleg\Controller\Index\Index as FirstIndex;
+use Amasty\BaharOleg\Controller\Index\Index as firstIndex;
+use Amasty\BaharOleg\Model\Config\ConfigProvider;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Customer\Model\Session;
 
-class Index extends FirstIndex
+
+
+class Index extends firstIndex
 {
+    /**
+     * @var Session
+     */
+    private $customerSession;
+
+    public function __construct(
+        ResultFactory $resultFactory,
+        ConfigProvider $configProvider,
+        Session $customerSession
+    )
+    {
+        parent::__construct($resultFactory, $configProvider);
+        $this->customerSession = $customerSession;
+    }
+
     public function execute()
     {
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        $customerSession = $objectManager->get('Magento\Customer\Model\Session');
-        if($customerSession->isLoggedIn()){
+        if($this->customerSession->isLoggedIn()){
             return parent::execute();
         }
         else{
